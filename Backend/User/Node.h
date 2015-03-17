7efdd41b6b6710ce76f56687cc3d18176c50617e
BjornNorgaard <bjornsnorgaard@gmail.com>
2015-03-17 16:21:46 +0100

    ifndef things

---
+++ Backend/User/Node.h
@@ -0,0 +1,19 @@
+#ifndef NODE_H_
+#define NODE_H_
+
+#include<cstdlib>
+
+template<typename T>
+class Node {
+public:
+	Node(const T& i = T(), Node<T>* n = NULL)
+		: info(i), next(n)
+	{
+
+	}
+
+	T info;
+	Node<T>* next;
+};
+
+#endif // NODE_H_
