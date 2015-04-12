5b126935f11f9ef5cea4a69a277962454322b6c9
BjornNorgaard <bjornsnorgaard@gmail.com>
2015-04-12 23:17:56 +0200

    cleaned up code and improved error handling

--- Server/ServerClient/ServerTest_App.js
+++ Server/ServerClient/ServerTest_App.js
@@ -3,7 +3,7 @@ var url = require('url');
 var port = 8000;
 
 var postOptions = {
-	host: '127.0.0.1',
+	host: '10.0.0.13',
 	port: port, 
 	path: '/duckmaster3001_application', 
 	method: 'GET',
@@ -19,12 +19,15 @@ var receive = function() {
 			
 		// Receive part of the data
 		result.on("data", function(data) {
-			p("request.on: data");	
+			p("receiving...");	
+			
+			// assembling full dataset
 			body += data;
 		})
 		
 		// Done receiving data, now saving
 		result.on("end", function() {
+			p("transfer complete!")
 			var msg = JSON.parse(body);
 			
 			console.log(msg);
@@ -33,8 +36,8 @@ var receive = function() {
 
 	httpRequest.end();
 	
+	// polling frequency in milliseconds
 	setTimeout(receive, 2000);
-	p("Sender");
 }
 
 var p = function(output) {
